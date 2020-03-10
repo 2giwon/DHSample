be18564e0f895d5fc54594cbedcde4d775275d08
middlefgr <2giwon@gmail.com>
2020-03-10 20:29:19 +0900

    #35 serializedName 추가

--- DHSample/remote/src/main/java/com/egiwon/remote/response/RemoteUserResponse.kt
+++ DHSample/remote/src/main/java/com/egiwon/remote/response/RemoteUserResponse.kt
@@ -1,13 +1,25 @@
 package com.egiwon.remote.response
 
 import com.egiwon.repository.model.UserDomainEntity
+import com.google.gson.annotations.SerializedName
 
 data class RemoteUserResponse(
+
+    @SerializedName("id")
     val id: String = "",
+
+    @SerializedName("avatarUrl")
     val avatarUrl: String = "",
+
+    @SerializedName("name")
     val name: String = "",
+
+    @SerializedName("score")
     val score: Double = 0.0,
+
+    @SerializedName("like")
     val like: Boolean = false
+
 ) : RemoteResponse
 
 fun RemoteUserResponse.mapToDomainEntity(): UserDomainEntity {
