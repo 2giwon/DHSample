dfddfebe9313cac71a4ae6615dd91720a7738788
2giwon <2giwon@gmail.com>
2020-03-10 11:47:04 +0900

    #33 koin 로그 디버그 일때만 적용하도록 수정

--- DHSample/app/src/main/java/com/egiwon/delieveryherosample/GithubApplication.kt
+++ DHSample/app/src/main/java/com/egiwon/delieveryherosample/GithubApplication.kt
@@ -7,14 +7,15 @@ import com.egiwon.remote.di.networkModule
 import com.egiwon.remote.di.remoteDataSourceModule
 import com.egiwon.repository.di.dataSourceModule
 import org.koin.android.ext.koin.androidContext
-import org.koin.android.ext.koin.androidLogger
+import org.koin.android.logger.AndroidLogger
 import org.koin.core.context.startKoin
+import org.koin.core.logger.EmptyLogger
 
 class GithubApplication : Application() {
     override fun onCreate() {
         super.onCreate()
         startKoin {
-            androidLogger()
+            logger(if (BuildConfig.DEBUG) AndroidLogger() else EmptyLogger())
             androidContext(this@GithubApplication)
             modules(
                 viewModelModule,
